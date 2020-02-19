d4060d2d8d4637de3dd157fb0be0aa1d3782bf3a
Hamorszky, Balazs <balihb@gmail.com>
2020-02-19 12:38:28 +0100

    bigdec

--- 1/5/Ot.java
+++ 1/5/Ot.java
@@ -19,6 +19,7 @@ class Ot {
         //int i = 4;
         for (int i = 1; i <= 4; i++) {
             double j = i / 2;
+            float f = i;
             double k = i;
             System.out.println();
             System.out.println("(double) (i / 2)");
@@ -43,6 +44,11 @@ class Ot {
             System.out.println((double) k / 2);
             // nincs szükség az " " utána írására, mindenképpen string lesz
             //System.out.println(i / 2 + " ");
+
+            System.out.println(f / 2);
+            System.out.println(i / 2f);
+
+            System.out.println(f / k);
         }
         //i = 4;
         System.out.println("Kiirtam " + 4 + " szamot");
@@ -57,114 +63,138 @@ https://docs.oracle.com/javase/8/docs/api/java/math/BigDecimal.html
 
 /*
 Bytecode:
+https://en.wikipedia.org/wiki/Java_bytecode_instruction_listings
 
-   L6
-    LINENUMBER 24 L6
+   L7
+    LINENUMBER 25 L7
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
     LDC "(double) (i / 2)"
     INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
-   L7
-    LINENUMBER 25 L7
+   L8
+    LINENUMBER 26 L8
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
     ILOAD 1
     ICONST_2
     IDIV
     I2D
     INVOKEVIRTUAL java/io/PrintStream.println (D)V
-   L8
-    LINENUMBER 26 L8
-    GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    LDC "i / (double) 2"
-    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
    L9
     LINENUMBER 27 L9
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    ILOAD 1
-    I2D
-    LDC 2.0
-    DDIV
-    INVOKEVIRTUAL java/io/PrintStream.println (D)V
+    LDC "i / (double) 2"
+    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
    L10
     LINENUMBER 28 L10
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    LDC "(double) i / 2"
-    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+    ILOAD 1
+    I2D
+    LDC 2.0
+    DDIV
+    INVOKEVIRTUAL java/io/PrintStream.println (D)V
    L11
     LINENUMBER 29 L11
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    ILOAD 1
-    I2D
-    LDC 2.0
-    DDIV
-    INVOKEVIRTUAL java/io/PrintStream.println (D)V
+    LDC "(double) i / 2"
+    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
    L12
     LINENUMBER 30 L12
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    LDC "i / 2."
-    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+    ILOAD 1
+    I2D
+    LDC 2.0
+    DDIV
+    INVOKEVIRTUAL java/io/PrintStream.println (D)V
    L13
     LINENUMBER 31 L13
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    ILOAD 1
-    I2D
-    LDC 2.0
-    DDIV
-    INVOKEVIRTUAL java/io/PrintStream.println (D)V
+    LDC "i / 2."
+    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
    L14
     LINENUMBER 32 L14
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    LDC "i / 2.0"
-    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+    ILOAD 1
+    I2D
+    LDC 2.0
+    DDIV
+    INVOKEVIRTUAL java/io/PrintStream.println (D)V
    L15
     LINENUMBER 33 L15
+    GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+    LDC "i / 2.0"
+    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+   L16
+    LINENUMBER 34 L16
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
     ILOAD 1
     I2D
     LDC 2.0
     DDIV
     INVOKEVIRTUAL java/io/PrintStream.println (D)V
-   L16
-    LINENUMBER 35 L16
-    GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    LDC "j"
-    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
    L17
     LINENUMBER 36 L17
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+    LDC "j"
+    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+   L18
+    LINENUMBER 37 L18
+    GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
     DLOAD 2
     INVOKEVIRTUAL java/io/PrintStream.println (D)V
-   L18
-    LINENUMBER 38 L18
-    GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    LDC "(double) j"
-    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
    L19
     LINENUMBER 39 L19
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    DLOAD 2
-    INVOKEVIRTUAL java/io/PrintStream.println (D)V
+    LDC "(double) j"
+    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
    L20
     LINENUMBER 40 L20
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    LDC "k / 2"
-    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+    DLOAD 2
+    INVOKEVIRTUAL java/io/PrintStream.println (D)V
    L21
     LINENUMBER 41 L21
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    DLOAD 4
-    LDC 2.0
-    DDIV
-    INVOKEVIRTUAL java/io/PrintStream.println (D)V
+    LDC "k / 2"
+    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
    L22
     LINENUMBER 42 L22
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    LDC "(double) k / 2"
-    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+    DLOAD 5
+    LDC 2.0
+    DDIV
+    INVOKEVIRTUAL java/io/PrintStream.println (D)V
    L23
     LINENUMBER 43 L23
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
-    DLOAD 4
+    LDC "(double) k / 2"
+    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+   L24
+    LINENUMBER 44 L24
+    GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+    DLOAD 5
     LDC 2.0
+    DDIV
+    INVOKEVIRTUAL java/io/PrintStream.println (D)V
+   L25
+    LINENUMBER 48 L25
+    GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+    FLOAD 4
+    FCONST_2
+    FDIV
+    INVOKEVIRTUAL java/io/PrintStream.println (F)V
+   L26
+    LINENUMBER 49 L26
+    GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+    ILOAD 1
+    I2F
+    FCONST_2
+    FDIV
+    INVOKEVIRTUAL java/io/PrintStream.println (F)V
+   L27
+    LINENUMBER 51 L27
+    GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+    FLOAD 4
+    F2D
+    DLOAD 5
     DDIV
     INVOKEVIRTUAL java/io/PrintStream.println (D)V
 */
--- 1/7/Het.java
+++ 1/7/Het.java
@@ -1,13 +1,14 @@
-class Het
-{
-    public static void main(String[] args)
-    {
-        if (args.length != 2)
-        {
+class Het {
+    public static void main(String[] args) {
+        if (args.length != 2) {
             System.out.println("nem elég paraméter");
         } else {
             int a = Integer.parseInt(args[0]);
             int b = Integer.parseInt(args[1]);
+
+            // https://docs.oracle.com/javase/tutorial/java/nutsandbolts/operators.html
+
+            System.out.println("a + b = " + a + b);
             System.out.println("a + b = " + (a + b));
             System.out.println("a - b = " + (a - b));
             System.out.println("a * b = " + (a * b));
@@ -22,8 +23,50 @@ class Het
 /*
 Bytecode:
 
+   L5
+    LINENUMBER 11 L5
+    GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+    NEW java/lang/StringBuilder
+    DUP
+    INVOKESPECIAL java/lang/StringBuilder.<init> ()V
+    LDC "a + b = "
+    INVOKEVIRTUAL java/lang/StringBuilder.append (Ljava/lang/String;)Ljava/lang/StringBuilder;
+    ILOAD 1
+    INVOKEVIRTUAL java/lang/StringBuilder.append (I)Ljava/lang/StringBuilder;
+    ILOAD 2
+    INVOKEVIRTUAL java/lang/StringBuilder.append (I)Ljava/lang/StringBuilder;
+    INVOKEVIRTUAL java/lang/StringBuilder.toString ()Ljava/lang/String;
+    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+   L6
+    LINENUMBER 12 L6
+    GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+    NEW java/lang/StringBuilder
+    DUP
+    INVOKESPECIAL java/lang/StringBuilder.<init> ()V
+    LDC "a + b = "
+    INVOKEVIRTUAL java/lang/StringBuilder.append (Ljava/lang/String;)Ljava/lang/StringBuilder;
+    ILOAD 1
+    ILOAD 2
+    IADD
+    INVOKEVIRTUAL java/lang/StringBuilder.append (I)Ljava/lang/StringBuilder;
+    INVOKEVIRTUAL java/lang/StringBuilder.toString ()Ljava/lang/String;
+    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
    L7
     LINENUMBER 13 L7
+    GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
+    NEW java/lang/StringBuilder
+    DUP
+    INVOKESPECIAL java/lang/StringBuilder.<init> ()V
+    LDC "a - b = "
+    INVOKEVIRTUAL java/lang/StringBuilder.append (Ljava/lang/String;)Ljava/lang/StringBuilder;
+    ILOAD 1
+    ILOAD 2
+    ISUB
+    INVOKEVIRTUAL java/lang/StringBuilder.append (I)Ljava/lang/StringBuilder;
+    INVOKEVIRTUAL java/lang/StringBuilder.toString ()Ljava/lang/String;
+    INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
+   L8
+    LINENUMBER 14 L8
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
     NEW java/lang/StringBuilder
     DUP
@@ -36,12 +79,12 @@ Bytecode:
     INVOKEVIRTUAL java/lang/StringBuilder.append (I)Ljava/lang/StringBuilder;
     INVOKEVIRTUAL java/lang/StringBuilder.toString ()Ljava/lang/String;
     INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
-   L8
-    LINENUMBER 14 L8
-    ILOAD 2
-    IFEQ L3
    L9
     LINENUMBER 15 L9
+    ILOAD 2
+    IFEQ L3
+   L10
+    LINENUMBER 16 L10
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
     NEW java/lang/StringBuilder
     DUP
@@ -56,8 +99,8 @@ Bytecode:
     INVOKEVIRTUAL java/lang/StringBuilder.append (D)Ljava/lang/StringBuilder;
     INVOKEVIRTUAL java/lang/StringBuilder.toString ()Ljava/lang/String;
     INVOKEVIRTUAL java/io/PrintStream.println (Ljava/lang/String;)V
-   L10
-    LINENUMBER 16 L10
+   L11
+    LINENUMBER 17 L11
     GETSTATIC java/lang/System.out : Ljava/io/PrintStream;
     NEW java/lang/StringBuilder
     DUP
