c2b2c70476567e9684716f79e1c60169c2bc7e7d
Hamorszky, Balazs <bhamorszky@chemaxon.com>
2020-03-11 14:12:53 +0100

    5 prep

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,21 @@
 rootProject.name = "java-gyak"
 
+include("1")
+include("1:1")
+include("1:2")
+include("1:3")
+include("1:4")
+include("1:5")
+include("1:6")
+include("1:7")
+
+include("2")
+include("2:Fel1")
+include("2:Fel2")
+include("2:Fel3")
+include("2:Fel4")
+include("2:Fel5")
+
 include("3")
 include("3:1")
 include("3:2")
@@ -8,3 +24,11 @@ include("3:4")
 include("3:6")
 include("3:7")
 include("3:7.1")
+
+//include("4:ropi")
+include("4:4")
+//include("4:5.0")
+include("4:5")
+
+include("5")
+include("5:1")
