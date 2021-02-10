4ab04a0027e88af3b77b4ed92803b45306c85c42
Hamorszky, Balazs <balihb@gmail.com>
2021-02-10 08:59:37 +0100

    20-21-2 init

---
+++ 20-21-2/01/03/Read.java
@@ -0,0 +1,6 @@
+class Read {
+	public static void main( String[] args ){
+		String rl = System.console().readLine();
+		System.out.println(rl);
+    }
+}
---
+++ 20-21-2/01/05/Print.java
@@ -0,0 +1,9 @@
+class Print {
+    public static void main(String[] args) {
+		double i = 4;
+        for(i = 1; i <= 4; i++ ) {
+            System.out.println(i/2);
+        }
+        System.out.println("Kiirtam " + i + " szamot");
+    }
+}
