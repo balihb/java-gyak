0834ea8e0bc066294d14c01a9f4d704d97034b22
Hamorszky, Balazs <balihb@gmail.com>
2021-02-17 01:42:23 +0100

    print fix

--- 20-21-2/01/05/Print.java
+++ 20-21-2/01/05/Print.java
@@ -1,9 +1,9 @@
 class Print {
     public static void main(String[] args) {
-		double i = 4;
+        double i = 4;
         for(i = 1; i <= 4; i++ ) {
             System.out.println(i/2);
         }
-        System.out.println("Kiirtam " + i + " szamot");
+        System.out.println("Kiirtam " + (i-1) + " szamot");
     }
 }
